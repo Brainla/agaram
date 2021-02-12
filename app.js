e9142e815f219b6f1d84c370f3cc359feeec5ab8
BrainLA <brainlapro@gmail.com>
2021-02-12 20:20:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const MongoClient = require("mongodb").MongoClient
 
 const app = express()
-const url = "mongodb://localhost:27017/agaramdb"
+const url = "mongodb+srv://agaram:e1d6yY6bAxsF4m7t@cluster0.t8joo.mongodb.net/agaram?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 3000;
 const path = require("path")
